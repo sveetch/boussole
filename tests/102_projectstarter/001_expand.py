7a1d6edd07ca9433db148a011fc45206d0dfffe0
David THENON <sveetch@gmail.com>
2016-11-19 01:38:27 +0100

    Added YAML backend to the projectstarter tests

--- tests/102_projectstarter/001_expand.py
+++ tests/102_projectstarter/001_expand.py
@@ -4,96 +4,89 @@ import json
 import pytest
 
 
-def test_001(projectstarter):
-    """project.expand: basic"""
+@pytest.mark.parametrize("sources,attempted", [
+    # basic
+    (
+        [
+            '.',
+            'settings.json',
+            'scss',
+            'css',
+            '/home/foo',
+        ],
+        (
+            '/home/foo',
+            '/home/foo/settings.json',
+            '/home/foo/scss',
+            '/home/foo/css',
+        ),
+    ),
+    # having absolute path for basedir
+    (
+        [
+            '/home/foo',
+            'settings.json',
+            'scss',
+            'css',
+            '/home/bar',
+        ],
+        (
+            '/home/foo',
+            '/home/foo/settings.json',
+            '/home/foo/scss',
+            '/home/foo/css',
+        ),
+    ),
+    # absolute basedir but unused because other paths are absolute also
+    (
+        [
+            '/home/bar',
+            '/home/foo/settings.json',
+            '/home/foo/scss',
+            '/home/foo/css',
+            '/home/meuh',
+        ],
+        (
+            '/home/bar',
+            '/home/foo/settings.json',
+            '/home/foo/scss',
+            '/home/foo/css',
+        ),
+    ),
+    # using non basic relative paths
+    (
+        [
+            '/home/foo',
+            "settings.json",
+            "sass/scss",
+            "../project/static/css",
+            '/home/bar',
+        ],
+        (
+            '/home/foo',
+            '/home/foo/settings.json',
+            '/home/foo/sass/scss',
+            '/home/project/static/css',
+        ),
+    ),
+    # mixin of everything before
+    (
+        [
+            '/home/foo',
+            "/home/bar/settings.json",
+            "sass/scss",
+            "../project/static/css",
+            '/home/meuh',
+        ],
+        (
+            '/home/foo',
+            '/home/bar/settings.json',
+            '/home/foo/sass/scss',
+            '/home/project/static/css',
+        ),
+    ),
+])
+def test_expand(projectstarter, sources, attempted):
+    results = projectstarter.expand(*sources)
 
-    results = projectstarter.expand(*[
-        '.',
-        'settings.json',
-        'scss',
-        'css',
-        '/home/foo',
-    ])
-
-    assert results == (
-        '/home/foo',
-        '/home/foo/settings.json',
-        '/home/foo/scss',
-        '/home/foo/css',
-    )
-
-
-def test_002(projectstarter):
-    """project.expand: having absolute path for basedir"""
-
-    results = projectstarter.expand(*[
-        '/home/foo',
-        'settings.json',
-        'scss',
-        'css',
-        '/home/bar',
-    ])
-
-    assert results == (
-        '/home/foo',
-        '/home/foo/settings.json',
-        '/home/foo/scss',
-        '/home/foo/css',
-    )
-
-
-def test_003(projectstarter):
-    """project.expand: absolute basedir but unused because other paths are absolute also"""
-
-    results = projectstarter.expand(*[
-        '/home/bar',
-        '/home/foo/settings.json',
-        '/home/foo/scss',
-        '/home/foo/css',
-        '/home/meuh',
-    ])
-
-    assert results == (
-        '/home/bar',
-        '/home/foo/settings.json',
-        '/home/foo/scss',
-        '/home/foo/css',
-    )
-
-
-def test_004(projectstarter):
-    """project.expand: using non basic relative paths"""
-
-    results = projectstarter.expand(*[
-        '/home/foo',
-        "settings.json",
-        "sass/scss",
-        "../project/static/css",
-        '/home/bar',
-    ])
-
-    assert results == (
-        '/home/foo',
-        '/home/foo/settings.json',
-        '/home/foo/sass/scss',
-        '/home/project/static/css',
-    )
-
-
-def test_004(projectstarter):
-    """project.expand: mixin of everything before"""
-
-    results = projectstarter.expand(*[
-        '/home/foo',
-        "/home/bar/settings.json",
-        "sass/scss",
-        "../project/static/css",
-        '/home/meuh',
-    ])
-
-    assert results == (
-        '/home/foo',
-        '/home/bar/settings.json',
-        '/home/foo/sass/scss',
-        '/home/project/static/css',
-    )
+    assert results == attempted
--- tests/102_projectstarter/003_commit.py
+++ tests/102_projectstarter/003_commit.py
@@ -4,58 +4,34 @@ import json
 import yaml
 import pytest
 
+@pytest.mark.parametrize("name,ext,module", [
+    ('json', 'json', json),
+    ('yaml', 'yml', yaml),
+])
+def test_commit_basic(projectstarter, temp_builds_dir, name, ext, module):
+    """Commit with basic values for every backends"""
+    tmp_dirname = 'projectstarter_commit_{}'.format(name)
+    settings_filename = "settings.{}".format(ext)
 
-def test_json(projectstarter, temp_builds_dir):
-    """project.init_project: Testing on basic values"""
-    basedir = temp_builds_dir.join('projectstarter_commit_json').strpath
+    basedir = temp_builds_dir.join(tmp_dirname).strpath
     os.makedirs(basedir)
 
     opts = (
         "scss",
         "css",
-        os.path.join(basedir, "settings.json"),
+        os.path.join(basedir, settings_filename),
         os.path.join(basedir, "scss"),
         os.path.join(basedir, "css"),
     )
 
     projectstarter.commit(*opts)
 
-    assert os.path.exists(os.path.join(basedir, "settings.json")) == True
+    assert os.path.exists(os.path.join(basedir, settings_filename)) == True
     assert os.path.exists(os.path.join(basedir, "scss")) == True
     assert os.path.exists(os.path.join(basedir, "css")) == True
 
-    with open(os.path.join(basedir, "settings.json"), "r") as fp:
-        assert json.load(fp) == {
-            'SOURCES_PATH': 'scss',
-            'TARGET_PATH': 'css',
-            "LIBRARY_PATHS": [],
-            "OUTPUT_STYLES": "nested",
-            "SOURCE_COMMENTS": False,
-            "EXCLUDES": []
-        }
-
-
-def test_yaml(projectstarter, temp_builds_dir):
-    """project.init_project: Testing on basic values"""
-    basedir = temp_builds_dir.join('projectstarter_commit_yaml').strpath
-    os.makedirs(basedir)
-
-    opts = (
-        "scss",
-        "css",
-        os.path.join(basedir, "settings.yml"),
-        os.path.join(basedir, "scss"),
-        os.path.join(basedir, "css"),
-    )
-
-    projectstarter.commit(*opts)
-
-    assert os.path.exists(os.path.join(basedir, "settings.yml")) == True
-    assert os.path.exists(os.path.join(basedir, "scss")) == True
-    assert os.path.exists(os.path.join(basedir, "css")) == True
-
-    with open(os.path.join(basedir, "settings.yml"), "r") as fp:
-        assert yaml.load(fp) == {
+    with open(os.path.join(basedir, settings_filename), "r") as fp:
+        assert module.load(fp) == {
             'SOURCES_PATH': 'scss',
             'TARGET_PATH': 'css',
             "LIBRARY_PATHS": [],
